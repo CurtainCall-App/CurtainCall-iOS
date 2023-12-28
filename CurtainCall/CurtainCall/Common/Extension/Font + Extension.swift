c2a84d6f0c70d3617ec7e7c28e4a770fa1858a49
mandos1995 <mandos19950620@gmail.com>
2023-12-28 10:52:15 +0900

    feat: Font Extension 작성

---
+++ CurtainCall/CurtainCall/Common/Extension/Font + Extension.swift
@@ -0,0 +1,23 @@
+//
+//  Font + Extension.swift
+//  CurtainCall
+//
+//  Created by 김민석 on 12/28/23.
+//
+
+import SwiftUI
+
+extension Font {
+    static let heading1: Font = .custom("SpoqaHanSansNeo-Bold", size: 26)
+    static let heading2: Font = .custom("SpoqaHanSansNeo-Bold", size: 22)
+    static let subTitle1: Font = .custom("SpoqaHanSansNeo-Bold", size: 20)
+    static let subTitle2: Font = .custom("SpoqaHanSansNeo-Bold", size: 18)
+    static let subTitle3: Font = .custom("SpoqaHanSansNeo-Bold", size: 17)
+    static let subTitle4: Font = .custom("SpoqaHanSansNeo-Bold", size: 16)
+    static let body1: Font = .custom("SpoqaHanSansNeo-Medium", size: 16)
+    static let body2: Font = .custom("SpoqaHanSansNeo-Medium", size: 15)
+    static let body3: Font = .custom("SpoqaHanSansNeo-Medium", size: 14)
+    static let body4: Font = .custom("SpoqaHanSansNeo-Medium", size: 13)
+    static let body5: Font = .custom("SpoqaHanSansNeo-Medium", size: 12)
+    static let caption: Font = .custom("SpoqaHanSansNeo-Regular", size: 10)
+}
