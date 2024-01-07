6ac58adf03c89c9485a6aed80ea0e2da8c0018ce
mandos1995 <mandos19950620@gmail.com>
2024-01-07 18:11:19 +0900

    feat: 애플 로그인 API 네트워크 통신 구현

--- CurtainCall/CurtainCall/Scenes/Login/Client/LoginClient.swift
+++ CurtainCall/CurtainCall/Scenes/Login/Client/LoginClient.swift
@@ -1,5 +1,5 @@
 //
-//  AppleLoginClient.swift
+//  LoginClient.swift
 //  CurtainCall
 //
 //  Created by 김민석 on 1/4/24.
@@ -7,19 +7,53 @@
 
 import UIKit
 import AuthenticationServices
+import Combine
 
 import ComposableArchitecture
+import Moya
 
-struct AppleLoginClient {
-    var signIn: () async throws -> String
+struct LoginClient {
+    var signInApple: () async throws -> String
+    var requestLogin: (String) async throws -> Bool
 }
 
-extension AppleLoginClient: DependencyKey {
+extension LoginClient: DependencyKey {
     static var liveValue = {
-        Self(signIn: signIn)
+        Self(
+            signInApple: signInApple,
+            requestLogin: requestLogin(idToken:)
+        )
     }()
     
-    static func signIn() async throws -> String {
+    static func requestLogin(idToken: String) async throws -> Bool {
+        let provider = MoyaProvider<LoginAPI>()
+        return try await withCheckedThrowingContinuation { continuation in
+            provider.request(.apple(idToken)) { result in
+                switch result {
+                case .success(let response):
+                    if response.statusCode == 404 {
+                        UserDefaults.standard[.idToken] = idToken
+                        continuation.resume(returning: false)
+                        return
+                    }
+                    do {
+                        let data = try JSONDecoder().decode(AuthenticationResponse.self, from: response.data)
+                        UserDefaults.standard[.accessToken] = data.accessToken
+                        UserDefaults.standard[.userId] = data.memberId
+                        UserDefaults.standard[.loginType] = .apple
+                        continuation.resume(returning: true)
+                    } catch {
+                        continuation.resume(throwing: NSError(domain: "DecodeError", code: 100))
+                    }
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+        
+    }
+    
+    static func signInApple() async throws -> (String) {
         let appleProvider = ASAuthorizationAppleIDProvider()
         let request = appleProvider.createRequest()
         request.requestedScopes = [.email]
@@ -53,7 +87,7 @@ extension AppleLoginClient: DependencyKey {
             if let crediential = authorization.credential as? ASAuthorizationAppleIDCredential,
                let identityToken = crediential.identityToken,
                let token = String(data: identityToken, encoding: .utf8) {
-                continuation?.resume(returning: token)
+                continuation?.resume(returning: (token))
             } else {
                 // MARK: Error처리
                 continuation?.resume(throwing: NSError(domain: "Apple Crediential Error", code: 1995))
@@ -67,8 +101,8 @@ extension AppleLoginClient: DependencyKey {
 }
 
 extension DependencyValues {
-    var appleLoginClient: AppleLoginClient {
-        get { self[AppleLoginClient.self] }
-        set { self[AppleLoginClient.self] = newValue }
+    var loginClient: LoginClient {
+        get { self[LoginClient.self] }
+        set { self[LoginClient.self] = newValue }
     }
 }
--- CurtainCall/CurtainCall/Scenes/Login/Feature/LoginFeature.swift
+++ CurtainCall/CurtainCall/Scenes/Login/Feature/LoginFeature.swift
@@ -8,33 +8,43 @@
 import Foundation
 
 import ComposableArchitecture
-import AuthenticationServices
 
 @Reducer
 struct LoginFeature {
     struct State: Equatable {
+        var loginType: LoginType = .apple
     }
     
     enum Action {
         case appleLoginTapped
         case idTokenResponse(String)
         case idTokenError(Error)
+        case requestLogin(Bool)
     }
     
-    @Dependency(\.appleLoginClient) var appleLoginClient
+    @Dependency(\.loginClient) var loginClient
     
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
             case .appleLoginTapped:
+                state.loginType = .apple
                 return .run { send in
-                    try await send(.idTokenResponse(self.appleLoginClient.signIn()))
+                    try await send(.idTokenResponse(self.loginClient.signInApple()))
                 }
             case .idTokenResponse(let token):
                 print("token: \(token)")
-                return .none
+                return .run { [loginType = state.loginType] send in
+                    if loginType == .apple {
+                        try await send(.requestLogin(self.loginClient.requestLogin(token)))
+                    }
+                    // TODO: 다른소셜로그인 처리
+                }
             case .idTokenError(let error):
                 print("error: \(error.localizedDescription)")
+                return .none
+            case .requestLogin(let islogin):
+                print("isLoggin: \(islogin)")
                 return .none
             }
         }
