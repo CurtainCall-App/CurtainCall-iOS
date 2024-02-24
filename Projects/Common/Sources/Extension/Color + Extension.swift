6d4eec024c04ce56e638a5f523273e659749f59e
mandos1995 <mandos19950620@gmail.com>
2024-02-24 14:34:38 +0900

    add: 자주사용하는 Color Extension 추가

--- Projects/Common/Sources/Extension/Color + Extension.swift
+++ Projects/Common/Sources/Extension/Color + Extension.swift
@@ -16,3 +16,19 @@ extension Color {
         self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
     }
 }
+
+extension Color {
+    static let primary1 = Color("hex0D1327")
+    static let gray1 = Color("hex1C1E21")
+    static let gray2 = Color("hex32363D")
+    static let gray3 = Color("hex464B51")
+    static let gray4 = Color("hex71757C")
+    static let gray5 = Color("hexA1A5AE")
+    static let gray6 = Color("hexC6C8CD")
+    static let gray7 = Color("hexE5E7EB")
+    static let gray8 = Color("hexF1F1F5")
+    static let gray9 = Color("hexF8F8F8")
+    static let systemRed = Color("hexFF334B")
+    static let systemGreen = Color("hex00C271")
+    static let systemYellow = Color("hexFFD600")
+}
