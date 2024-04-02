6618e45f0a86866bdb8b282063f2264ef09a4941
mandos1995 <mandos19950620@gmail.com>
2024-04-02 22:37:07 +0900

    feat: 파티원 없는 UI 구현

--- Projects/Party/Sources/Views/PartyView.swift
+++ Projects/Party/Sources/Views/PartyView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+import Common
+
 import ComposableArchitecture
 
 public struct PartyView: View {
@@ -17,8 +19,57 @@ public struct PartyView: View {
     }
     
     public var body: some View {
-        VStack {
-            Text("Party")
+        ZStack {
+            Color.gray8
+                .ignoresSafeArea(.container, edges: .top)
+            VStack {
+                topView
+                emptyView
+            }
+            VStack {
+                Spacer()
+                recruitMemberButton
+                    .padding(.bottom, 20)
+                    .padding(.horizontal, 20)
+            }
         }
+        .toolbar(.hidden)
+    }
+    
+    private var topView: some View {
+        HStack {
+            Text("파티원")
+                .font(.heading2)
+                .padding(.leading, 20)
+            Spacer()
+            Image(asset: CommonAsset.iconCalendar24px)
+                .padding(.trailing, 16)
+                .padding(.vertical, 10)
+            Image(asset: CommonAsset.iconSearch24px)
+                .padding(.trailing, 10)
+                .padding(.vertical, 10)
+        }
+    }
+    
+    private var emptyView: some View {
+        VStack {
+            Spacer()
+            Image(asset: CommonAsset.emptyParty60px)
+            Text("모집 중인 파티원이 없어요!")
+                .font(.body2_SB)
+                .foregroundStyle(Color.primary1)
+                .padding(.top, 16)
+            Spacer()
+        }
+    }
+    
+    private var recruitMemberButton: some View {
+        Text("파티원 모집하기")
+            .font(.subTitle4)
+            .foregroundStyle(.white)
+            .frame(maxWidth: .infinity)
+            .frame(height: 55)
+            .background(Color.primary1)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
