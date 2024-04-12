babade7cdf4b8f593806370fb18b0c860d70a42e
mandos1995 <mandos19950620@gmail.com>
2024-04-12 21:03:27 +0900

    feat: Step View 구현

--- Projects/Party/Sources/Views/PartyRecruitView.swift
+++ Projects/Party/Sources/Views/PartyRecruitView.swift
@@ -22,7 +22,8 @@ public struct PartyRecruitView: View {
     public var body: some View {
         VStack {
             topView
-            Text("테스트")
+            stepView
+                .padding(.top, 20)
             Spacer()
         }
         .toolbar(.hidden)
@@ -42,4 +43,33 @@ public struct PartyRecruitView: View {
         .padding(.horizontal, 16)
         .frame(height: 44)
     }
+    
+    private var stepView: some View {
+        switch store.viewType {
+        case .step1: Image(asset: CommonAsset.partyRecruitProgressStep1)
+        case .step2: Image(asset: CommonAsset.partyRecruitProgressStep2)
+        case .step3: Image(asset: CommonAsset.partyRecruitProgressStep3)
+        }
+    }
+    
+    private var showView: some View {
+        VStack {
+            Text("작품을 선택해주세요")
+                .foregroundStyle(.black)
+                .font(.subTitle4)
+                .padding(.top, 30)
+            
+        }
+    }
+    
+    private func makeShowTypeButton(type: ShowType) -> some View {
+        Text(type.title)
+            .font(.body2_SB)
+            .foregroundStyle(store.selectedShowType == type ? Color.white : Color.gray6)
+            .padding(.horizontal, 11)
+            .padding(.vertical, 4)
+            .background(store.selectedShowType == type ? Color.primary1 : Color.gray9)
+            .clipShape(RoundedRectangle(cornerRadius: 30))
+    }
+    
 }
