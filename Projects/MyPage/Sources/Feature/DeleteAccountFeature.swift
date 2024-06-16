f5b86f507d6c04756b08f7f7e8cf581267f787c9
mandos1995 <mandos19950620@gmail.com>
2024-06-16 13:57:47 +0900

    feat: 회원 탈퇴 페이지 UI 구현

--- Projects/MyPage/Sources/Feature/DeleteAccountFeature.swift
+++ Projects/MyPage/Sources/Feature/DeleteAccountFeature.swift
@@ -14,18 +14,54 @@ import ComposableArchitecture
 @Reducer
 public struct DeleteAccountFeature {
     
+    public enum DeleteAccountType: CaseIterable {
+        case 기록삭제
+        case 이용불편
+        case 타서비스
+        case 사용빈도
+        case 앱기능문제
+        case 기타
+        
+        public var title: String {
+            switch self {
+            case .기록삭제: return "기록을 삭제하기 위해"
+            case .이용불편: return "이용이 불편하고 장애가 잦아서"
+            case .타서비스: return "타 서비스가 더 좋아서"
+            case .사용빈도: return "사용빈도가 낮아서"
+            case .앱기능문제: return "앱 기능이 유용하지 않아서"
+            case .기타: return "기타"
+            }
+        }
+    }
+    
     @ObservableState
     public struct State: Equatable {
         public init() { }
+        var content: String = ""
+        var deleteAccountType: DeleteAccountType?
+        var isEnableDeleteAccount: Bool = false
     }
     
-    public enum Action {
-        
+    public enum Action: BindableAction {
+        case binding(BindingAction<State>)
+        case check(DeleteAccountType)
+        case didTappedDeleteAccount
     }
     
     public var body: some ReducerOf<Self> {
+        BindingReducer()
+        
         Reduce { state, action in
-            return .none
+            switch action {
+            case .binding:
+                return .none
+            case .check(let type):
+                state.deleteAccountType = type
+                state.isEnableDeleteAccount = true
+                return .none
+            case .didTappedDeleteAccount:
+                return .none
+            }
         }
     }
 }
--- Projects/MyPage/Sources/Views/DeleteAccountView.swift
+++ Projects/MyPage/Sources/Views/DeleteAccountView.swift
@@ -15,26 +15,110 @@ struct DeleteAccountView: View {
     
     @Environment(\.dismiss) var dismiss
     
-    private let store: StoreOf<DeleteAccountFeature>
+    @Bindable private var store: StoreOf<DeleteAccountFeature>
     
     init(store: StoreOf<DeleteAccountFeature>) {
         self.store = store
     }
     
     var body: some View {
-        VStack {
-            
-        }
-        .navigationTitle("계정 삭제")
-        .navigationBarBackButtonHidden()
-        .navigationBarTitleDisplayMode(.inline)
-        .toolbar {
-            ToolbarItem(placement: .topBarLeading) {
-                Image(asset: CommonAsset.navigationBackIcon)
-                    .onTapGesture {
-                        dismiss()
+        ZStack {
+            ScrollView {
+                VStack(spacing: 0) {
+                    HStack {
+                        Text("커튼콜을 떠나시나요?")
+                            .font(.subTitle1)
+                            .foregroundStyle(.black)
+                        Spacer()
                     }
+                    .padding(.top, 30)
+                    HStack {
+                        Text("계정을 삭제하려는 이유를 알려주세요.")
+                            .font(.body3)
+                            .foregroundStyle(Color.gray4)
+                        Spacer()
+                    }
+                    .padding(.top, 14)
+                    .padding(.bottom, 40)
+                    checkViews
+                    
+                    Spacer()
+                }
+                .padding(.horizontal, 20)
+                
+                Spacer().frame(height: 70)
+            }
+            .navigationTitle("계정 삭제")
+            .navigationBarBackButtonHidden()
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Image(asset: CommonAsset.navigationBackIcon)
+                        .onTapGesture {
+                            dismiss()
+                        }
+                }
+            }
+            VStack {
+                Spacer()
+                RectangleBottomButton(isEnable: $store.isEnableDeleteAccount, text: "다음") {
+                    store.send(.didTappedDeleteAccount)
+                }
+                .padding(.horizontal, 20)
+                .padding(.bottom, 10)
             }
         }
+    }
+    
+    private var checkViews: some View {
+        VStack(spacing: 20) {
+            ForEach(DeleteAccountFeature.DeleteAccountType.allCases, id: \.self) { type in
+                HStack(spacing: 10) {
+                    Image(asset: store.deleteAccountType == type ? CommonAsset.checkOffIcon20px : CommonAsset.checkOnIcon20px)
+                        .onTapGestureRectangle {
+                            store.send(.check(type))
+                        }
+                    Text(type.title)
+                        .font(.body2_SB)
+                        .foregroundStyle(Color.gray2)
+                    Spacer()
+                }
+            }
+            if store.deleteAccountType == .기타 {
+                TextEditor(text: $store.content)
+                    .font(.body2_M)
+                    .foregroundStyle(.black)
+                    .scrollContentBackground(.hidden)
+                    .background(.clear)
+                    .background(alignment: .topLeading) {
+                        TextEditor(text: .constant(store.content.isEmpty ? "자유롭게 작성해주세요." : ""))
+                            .font(.body2_M)
+                            .foregroundStyle(Color.gray6)
+                            .scrollContentBackground(.hidden)
+                            .background(.clear)
+                    }
+                    .padding(.horizontal, 14)
+                    .padding(.vertical, 12)
+                
+                    .frame(maxWidth: .infinity)
+                    .frame(minHeight: 130)
+                    .background(Color.gray9)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding(.top, 12)
+                
+                if store.content.count > 500 {
+                    HStack {
+                        Text("500자 이내로 작성해주세요")
+                            .font(.body3)
+                            .foregroundStyle(.red)
+                        Spacer()
+                    }
+                    .padding(.top, 12)
+                }
+            }
+            
+            
+        }
+        
     }
 }
