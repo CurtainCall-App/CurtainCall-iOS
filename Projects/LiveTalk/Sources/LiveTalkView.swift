2763a87ba303aa9ebc5497fdffd0cf91ddc0cf7c
mandos1995 <mandos19950620@gmail.com>
2024-08-10 15:39:37 +0900

    feat: LiveTalk 모듈 생성

---
+++ Projects/LiveTalk/Sources/LiveTalkView.swift
@@ -0,0 +1,16 @@
+//
+//  LiveTalkView.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 김민석 on 8/10/24.
+//
+
+import SwiftUI
+
+public struct LiveTalkView: View {
+    public init() { }
+    
+    public var body: some View {
+        Text("라이브톡")
+    }
+}
